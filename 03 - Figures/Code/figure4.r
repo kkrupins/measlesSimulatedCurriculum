3818b1d7f9020a2b4d3a343318b890b516f56fa9
kkrupins <kkrupins@umich.edu>
2026-08-05 17:27:41 -0400

    Update figure4.r

--- 03 - Figures/Code/figure4.r
+++ 03 - Figures/Code/figure4.r
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(readxl)
 
-source("./01 - Functions/plot4.R")
+source("./01 - Functions/plotFig4.R")
 
 dat <- read_excel("./02 - Input/Written Feedback/codedResponses_2026.xlsx") %>%
   select(id, primary, secondary, tertiary, quatrinary, quinary) %>%
